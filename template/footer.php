16f9308cd71db26f42a1c89191da108e9ae18786
AstoMiwanda <astomiwanda08@gmail.com>
2018-08-06 10:21:04 +0700

    fix bug footer

--- template/footer.php
+++ template/footer.php
@@ -1,14 +1,14 @@
-<link rel="stylesheet" href="http://localhost/tracer-study/assets/css/template.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/template.css">
 
 <div id="footer">
 	<div class="footer flex flex-spaceBetween flex-vcenter">
 		<h1>SMK Telkom Purwokerto</h1>
 		<div class="flex flex-vcenter">
 			<h2>Copyright</h2>
-			<img src="http://localhost/tracer-study/assets/img/cp.png" class="img">
+			<img src="<?php echo base_url(); ?>assets/img/cp.png" class="img">
 			<h2>HumasStematel</h2>
 		</div>
 	</div>
 </div>
 
-<script type="text/javascript" src="http://localhost/tracer-study/assets/js/template.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/template.js"></script>
